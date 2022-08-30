41ef62a4fdb8fb2a850f00eaf1b543ecc02292e8
Rulon <Rulon@KOMPUTER>
2022-08-30 14:29:15 +0700

    Добавьте файлы проекта.

---
+++ Gachi_gym/GachiDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gachi_gym
+{
+    public class GachiDB
+    {
+        public int Exercise_id { get; set; }
+        public string ExerciseName { get; set; }
+        public DateTime Exercise_date { get; set; } = DateTime.Today;
+        public bool IsDone { get; set; } = false;
+        public int Repetition { get; set; }
+        public int Approach { get; set; }
+    }
+
+}
---
+++ Gachi_gym/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gachi_gym
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void DataGridGachi_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+
+        }
+
+        private void Button_add(object sender, RoutedEventArgs e)
+        {
+            var ListOfGachi = new List<GachiDB>();
+
+            var GachiMen = new GachiDB()
+            {
+                Exercise_id = Convert.ToInt32(id_tb.Text),
+                ExerciseName = Nametb.Text,
+                IsDone = false,
+                Repetition = Convert.ToInt32(Repetition_tb.Text),
+                Approach = Convert.ToInt32(Approach_tb.Text) 
+            };
+            ListOfGachi.Add(GachiMen);
+            
+            DataGridGachi.ItemsSource = ListOfGachi;
+        }
+
+        private void Button_del(object sender, RoutedEventArgs e)
+        {
+            var ListOfGachi = new List<GachiDB>();
+            if (DataGridGachi.SelectedItem is GachiDB selectedgachi)
+            {
+                GachiDB gac = ListOfGachi.Where(c => c.Exercise_id == selectedgachi.Exercise_id).First();
+            }
+        }
+
+        private void Button_red(object sender, RoutedEventArgs e)
+        {
+            var ListOfGachi = new List<GachiDB>();
+            if (DataGridGachi.SelectedItem is GachiDB selectedgachi)
+            {
+                var GachiMen = new GachiDB()
+                {
+                    Exercise_id = Convert.ToInt32(id_tb.Text),
+                    ExerciseName = Nametb.Text,
+                    IsDone = false,
+                    Repetition = Convert.ToInt32(Repetition_tb.Text),
+                    Approach = Convert.ToInt32(Approach_tb.Text)
+                };
+                ListOfGachi.Add(GachiMen);
+            }
+        }
+    }
+}
